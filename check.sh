727274d2a6dae1f70e662d27f54f99b4d4135e96
KO Myung-Hun <komh@chollian.net>
2017-10-15 15:31:28 +0900

    check.sh: check the size of .err file as well
    If internal error occurs, mksh returns as if it succeeded. To
    distinguish these cases, check if the size of .err file is 0.
    
        modified:   check.sh

--- check.sh
+++ check.sh
@@ -1,6 +1,11 @@
 test_programs=$(ls *.t)
 
 for t in $test_programs; do
-  "$SH" "$t" > "$t.out" 2> "$t.err" &&
-    { rm "$t.out" "$t.err"; echo "PASS: $t"; } || echo "FAIL: $t"
+  if "$SH" "$t" > "$t.out" 2> "$t.err" &&
+     test "$(stat -c%s "$t.err")" = 0 ; then
+    rm "$t.out" "$t.err"
+    echo "PASS: $t"
+  else
+    echo "FAIL: $t"
+  fi
 done
